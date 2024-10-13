cb3522297c2e2e4f9abf64be4e4da2fcfbd3ba9d
nhadhoud <98334800+nhadhoud@users.noreply.github.com>
2024-10-13 17:14:31 +0100

    Update ObjFileLoader.java

--- ObjFileLoader.java
+++ ObjFileLoader.java
@@ -7,24 +7,26 @@ public class ObjFileLoader {
     public static Shape loadShape(String fileName) throws FileNotFoundException {
         ArrayList<float[]> vertices = new ArrayList<>();
         ArrayList<int[]> edges = new ArrayList<>();
-        
+
         File myObj = new File(fileName);
         Scanner myReader = new Scanner(myObj);
         while (myReader.hasNextLine()) {
             String line = myReader.nextLine();
             if (line.startsWith("v ")) {
                 String[] parts = line.split(" ");
-                float[] vertex = new float[4]; // vertices include a w coordinate for projection
-                for (int i = 1; i < 4; i++) { // skip "v"
+                float[] vertex = new float[4]; //vertices include a w coordinate for projection
+                for (int i = 1; i < 4; i++) { //skip "v"
                     vertex[i - 1] = Float.parseFloat(parts[i]);
                 }
                 vertex[3] = 1f; // w coordinate
                 vertices.add(vertex);
-            } else if (line.startsWith("f ")) {
+            } 
+            else if (line.startsWith("f ")) {
                 String[] parts = line.split(" ");
                 int[] face = new int[parts.length - 1];
-                for (int i = 1; i < parts.length; i++) {
-                    face[i - 1] = Integer.parseInt(parts[i].split("/")[0]) - 1; // skip "f"
+                for (int i = 1; i < parts.length; i++) { //skip "f"
+                    String[] vertexReferences = parts[i].split("/");
+                    face[i - 1] = Integer.parseInt(vertexReferences[0]) - 1; //make 0 indexed
                 }
                 faceToEdges(face, edges);
             }
@@ -37,11 +39,10 @@ public class ObjFileLoader {
     }
 
     private static void faceToEdges(int[] face, ArrayList<int[]> edges) {
-        for (int i = 0; i < face.length - 1; i++) {
+        for (int i = 0; i < face.length; i++) {
             int startVertexIndex = face[i];
-            int endVertexIndex = face[(i + 1)];
-            int[] edge = {startVertexIndex, endVertexIndex};
-            edges.add(edge);
+            int endVertexIndex = face[(i + 1) % face.length]; //faces store triangles so connect first to last
+            edges.add(new int[]{startVertexIndex, endVertexIndex});
         }
     }
 }
